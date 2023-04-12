69411d2d2ef8081caef75dfad4d3cb9b0a996bdf
saki4555 <saki999v@gmail.com>
2023-04-12 15:22:46 +0600

    eventhandler added for see all jobs button

--- src/Components/FeaturedJob/FeaturedJob.jsx
+++ src/Components/FeaturedJob/FeaturedJob.jsx
@@ -4,19 +4,40 @@ import SingleFeaturedJob from '../SingleFeaturedJob/SingleFeaturedJob';
 const FeaturedJob = () => {
 
     const [featuredJobs, setFeaturedJob] = useState([]);
+    const [temp, setTemp] = useState([]);
+    const [seeAll, setAll] = useState(false);
 
     useEffect(() => {
         fetch('featured.json')
             .then(res => res.json())
-            .then(data => setFeaturedJob(data));
+            .then(data => {
+                setFeaturedJob(data);
+                setTemp(data);
+
+            });
     }, [])
+
+    useEffect(() => {
+        if (!seeAll) {
+            const curJobs = featuredJobs.slice(0, 4);
+            setFeaturedJob(curJobs);
+        } else {
+            setFeaturedJob(temp);
+        }
+    }, [seeAll, temp])
+
+    console.log(featuredJobs);
+
+
+
+
     return (
         <div>
             <div className='text-center p-1 lg:p-0'>
                 <h1 className='text-5xl font-bold mb-4'>Featured Jobs</h1>
                 <p className='text-[#757575] font-medium mb-4 lg:mb-16'>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
-            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 my-container p-1 lg:p-0'>
+            <div className='grid grid-cols-1 lg:grid-cols-2 gap-4 my-container p-1 lg:p-0 mb-5'>
                 {
                     featuredJobs.map(featuredJob => <SingleFeaturedJob
                         key={featuredJob.id}
@@ -24,9 +45,13 @@ const FeaturedJob = () => {
                     ></SingleFeaturedJob>)
                 }
             </div>
-            <div className='text-center my-4'>
-            <button className='btn-bg banner-btn btn-animation'>See All Jobs</button>
-            </div>
+
+            {
+                !seeAll && (<div className='text-center my-4'>
+                    <button onClick={() => setAll(true)} className='btn-bg banner-btn btn-animation'>See All Jobs</button>
+                </div>)
+            }
+
         </div>
     );
 };
